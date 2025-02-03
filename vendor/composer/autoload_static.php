5ba7eb194475305b2a539362ea12a88066189e5d
Jacob Ransom <jacob@innocraft.com>
2025-02-04 10:46:53 +1300

    Updating dependency to eliminate deprecation warning

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,7 +6,15 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit150f3641fba2aa7f9c7e1c8d776f657c
 {
+    public static $files = array (
+        '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
+    );
+
     public static $prefixLengthsPsr4 = array (
+        'P' => 
+        array (
+            'Psr\\Container\\' => 14,
+        ),
         'F' => 
         array (
             'Faker\\' => 6,
@@ -14,9 +22,13 @@ class ComposerStaticInit150f3641fba2aa7f9c7e1c8d776f657c
     );
 
     public static $prefixDirsPsr4 = array (
+        'Psr\\Container\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/container/src',
+        ),
         'Faker\\' => 
         array (
-            0 => __DIR__ . '/..' . '/fzaninotto/faker/src/Faker',
+            0 => __DIR__ . '/..' . '/fakerphp/faker/src/Faker',
         ),
     );
 
